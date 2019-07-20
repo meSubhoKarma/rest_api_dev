2b72cb8cad4515da7dec928dfa261c6ed19f8794
Subho Karmakar <isubhokarma@gmail.com>
2019-07-21 04:35:42 +0530

    Update post by Id

--- routes/posts.js
+++ routes/posts.js
@@ -82,4 +82,24 @@ router.delete("/:postId", async (req, res) => {
   }
 });
 
+// Update a post
+router.patch("/:postId", async (req, res) => {
+  try {
+    const updatedPost = await Post.updateOne(
+      { _id: req.params.postId },
+      {
+        $set: {
+          title: req.body.title,
+          description: req.body.description
+        }
+      }
+    );
+    res.json(updatedPost);
+  } catch (err) {
+    res.json({
+      message: err
+    });
+  }
+});
+
 module.exports = router;
